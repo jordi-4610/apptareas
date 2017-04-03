2f3ef5b85b8177c7d55ad1b2ab1a7338ba90c867
Jordi Alcudia <jordialcudia@gmail.com>
2017-04-03 21:05:16 +0200

    Añadir heroku

--- models/models.js
+++ models/models.js
@@ -1,10 +1,25 @@
 var path = require('path');
+
+var DB_name  = (dc40pise18rf8r);
+var user     = (hqkdcxsslgpnyt);
+var pwd      = (8f302272b277824dc8d23e815a68934ca878fbf79f2487a7adf09b0387a39548);
+var protocol = (Postgres);
+var dialect  = (Postgres);
+var port     = (5432);
+var host     = (ec2-23-23-237-68.compute-1.amazonaws.com);
+var storage  = process.env.DATABASE_STORAGE;
+
+
 // Cargar el modelo ORM
 var Sequelize = require('sequelize');
 //Uso de la BBDD SQLite
-var sequelize = new Sequelize(null, null, null, 
-  { dialect:  "sqlite",
-    storage:  "tblsqlite.sqlite"
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
   }      
 );
 //Importación al objeto sequelize la tabla a Tbltareas que esta en tbltareas.js
